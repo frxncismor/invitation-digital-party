069b782a4a749cd48d2a2fefdb8da7a560c3bead
Francisco Moreno <frxncismor@gmail.com>
2024-05-19 20:46:29 -0600

    update cupcake version

--- src/app/pages/invitation/invitation.component.ts
+++ src/app/pages/invitation/invitation.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SupabaseService } from '../../services/supabase.service';
-import { Invitation, xvThemes, LayoutComponent, HeroComponent, InvitationComponent as CupcakeInvitationComponent} from '@digital-party/cupcake';
+import { Invitation, xvThemes, LayoutComponent, HeroComponent, InvitationComponent as CupcakeInvitationComponent, ItineraryComponent, ItineraryEventLocationComponent, FooterComponent} from '@digital-party/cupcake';
 import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-invitation',
   standalone: true,
-  imports: [LayoutComponent, HeroComponent, CupcakeInvitationComponent],
+  imports: [LayoutComponent, HeroComponent, CupcakeInvitationComponent, ItineraryEventLocationComponent, FooterComponent ],
   templateUrl: './invitation.component.html',
   styleUrl: './invitation.component.scss'
 })
@@ -19,7 +19,7 @@ export class InvitationComponent implements OnInit {
     theme: xvThemes.XV1,
     celebrated: {
       name: 'Dana',
-      eventDate: DateTime.fromISO("2024-05-31"),
+      eventDate: DateTime.fromISO("2024-05-31T17:00:00"),
       heroBgImg: 'https://images.pexels.com/photos/19726278/pexels-photo-19726278/free-photo-of-mujer-modelo-maqueta-ramo-de-flores.jpeg',
       invitationBgImg: 'https://adagbrjzndukouwqcknu.supabase.co/storage/v1/object/public/design_images/xv2/pexels-evie-shaffer-2395249.jpg'
     },
@@ -30,6 +30,7 @@ export class InvitationComponent implements OnInit {
       will_assist: null
     },
     event: {
+      timeStart: '5:00 pm',
       location: {
         url: 'https://maps.app.goo.gl/shYHz6kActShMXd58',
         placeName : '"El portón de Santa Fé"',
@@ -38,6 +39,22 @@ export class InvitationComponent implements OnInit {
           indications: 'Entre la Universidad Santa Fe y la caseta de cobro Cervera.',
           city: 'Guanajuato, Guanajuato.'
         }
+      },
+      itinerary: {
+        events: [
+          {
+            name: 'Estudiantina',
+            time: '6:00 pm'
+          },
+          {
+            name: 'Vals',
+            time: '7:00 pm'
+          },
+          {
+            name: 'Animador',
+            time: '8:30 pm'
+          },
+        ]
       }
     }
   }
